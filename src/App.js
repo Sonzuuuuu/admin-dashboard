e6c95bb598eec04ed1a16b35aa4f94d8b6236e60
Alberson Alejandrino <sonzualejandrino@gmail.com>
2022-11-01 22:00:56 +0800

    added Pages section

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ import './App.css';
 
 import { useStateContext } from './contexts/ContextProvider';
 
-
-
 const App = () => {
   const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
 
@@ -24,12 +22,15 @@ const App = () => {
   }, []);
 
   return (
-    <div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
-        <div className='flex relative dark:bg-main-dark-bg'>
-          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
-            <TooltipComponent content="Settings" position="Top">
-            <button
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+            <TooltipComponent
+              content="Settings"
+              position="Top"
+            >
+              <button
                 type="button"
                 onClick={() => setThemeSettings(true)}
                 style={{ background: currentColor, borderRadius: '50%' }}
@@ -37,49 +38,48 @@ const App = () => {
               >
                 <FiSettings />
               </button>
+
             </TooltipComponent>
           </div>
           {activeMenu ? (
-            <div className="w-72 fixed sidebar
-            dark:bg-secondary-dark-bg
-            bg-white" >
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
               <Sidebar />
             </div>
           ) : (
-            <div className='w-0 
-            dark:bg-secondary-dark-bg'>
+            <div className="w-0 dark:bg-secondary-dark-bg">
               <Sidebar />
             </div>
           )}
-          <div className={`dark:bg-main-bg bg-main-bg 
-          min-h-screen w-full ${ activeMenu ? 
-          'md-ml-72' : 'flex-2'}`
-          }>
-            <div className="fixed md:static
-            bg-main-bg dark:bg-main-dark-bg
-            navbar w-full">
+          <div
+            className={
+              activeMenu
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+            }
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
               <Navbar />
             </div>
-          
-
             <div>
-              <Routes>
-                {/* Dashboard */}
-                <Route path="/" element={<Ecommerce />} />
-                <Route path="/ecommerce" element={<Ecommerce />} />
+              {themeSettings && (<ThemeSettings />)}
 
-                {/* Pages */}
+              <Routes>
+                {/* dashboard  */}
+                <Route path="/" element={(<Ecommerce />)} />
+                <Route path="/ecommerce" element={(<Ecommerce />)} />
+
+                {/* pages  */}
                 <Route path="/orders" element={<Orders />} />
                 <Route path="/employees" element={<Employees />} />
                 <Route path="/customers" element={<Customers />} />
 
-                {/* Apps */}
+                {/* apps  */}
                 <Route path="/kanban" element={<Kanban />} />
                 <Route path="/editor" element={<Editor />} />
                 <Route path="/calendar" element={<Calendar />} />
                 <Route path="/color-picker" element={<ColorPicker />} />
 
-                {/* Charts */}
+                {/* charts  */}
                 <Route path="/line" element={<Line />} />
                 <Route path="/area" element={<Area />} />
                 <Route path="/bar" element={<Bar />} />
@@ -88,9 +88,9 @@ const App = () => {
                 <Route path="/color-mapping" element={<ColorMapping />} />
                 <Route path="/pyramid" element={<Pyramid />} />
                 <Route path="/stacked" element={<Stacked />} />
+
               </Routes>
             </div>
-            <Footer />
           </div>
         </div>
       </BrowserRouter>
@@ -98,4 +98,4 @@ const App = () => {
   );
 };
 
-export default App
+export default App;
